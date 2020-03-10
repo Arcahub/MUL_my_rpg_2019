f425e373ef5a68143455d32e24abafe5266b0f01
Arcarox <arcarox@localhost.localdomain>
2020-03-10 22:16:31 +0100

    [ADD] | first commit

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+##
+## EPITECH PROJECT, 2020
+## minishell1
+## File description:
+## deploy
+##
+
+ssh-keyscan -H git.epitech.eu >> ~/.ssh/known_hosts
+git push deploy master
---
+++ src/main.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2020
+** minishell1
+** File description:
+** main
+*/
+
+int main(void)
+{
+    return (84);
+}
