5f70f2fae27c2152d21f125417694bfcb2cd193a
Arcarox <arcarox@localhost.localdomain>
2020-03-18 16:46:53 +0100

    [MOD] | fix deploy

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,8 @@
 ## deploy
 ##
 
-NAME=$TRAVIS_REPO_SLUG
-REPO_NAME= $(echo $NAME | awk -F'/' '{print $2}')
-echo $REPO_NAME
+NAME='Arcahub/MYL_my_rpg_2019'
+REPO_NAME=$(echo -n $NAME | awk -F'/' '{print $2}')
 
 ssh-keyscan -H git.epitech.eu >> ~/.ssh/known_hosts
 git push git@git.epitech.eu:/guillaume.mailhol@epitech.eu/$REPO_NAME master
