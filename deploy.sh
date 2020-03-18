04a3d8a91100b0c46d445df1a18474b77b6bc162
Arcarox <arcarox@localhost.localdomain>
2020-03-18 16:26:29 +0100

    [MOD] | push to show travis env var

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,9 @@
 ##
 
 NAME=$TRAVIS_REPO_SLUG
+echo $TRAVIS_REPO_SLUG
 REPO_NAME=echo $NAME | awk -F'/' '{print $2}'
+echo $REPO_NAME
 
 ssh-keyscan -H git.epitech.eu >> ~/.ssh/known_hosts
 git push git@git.epitech.eu:/guillaume.mailhol@epitech.eu/$REPO_NAME master
