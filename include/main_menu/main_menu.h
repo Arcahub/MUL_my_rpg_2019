8ebc687b4426b7e277b83c23f388bec65e3274cc
Arcarox <arcarox@localhost.localdomain>
2020-04-01 14:58:49 +0200

    [MOD] | fix warnings

--- include/main_menu/main_menu.h
+++ include/main_menu/main_menu.h
@@ -13,5 +13,6 @@
 scene_t *rpg_main_menu_create_scene(game_t *game);
 void rpg_main_menu_draw_scene(scene_t *scene, game_t *game, sfRenderWindow *window);
 void rpg_main_menu_bind_event(scene_t *scene);
+game_object_t *rpg_main_menu_create_buttons(void);
 
 #endif /* !MAIN_MENU_H_ */
