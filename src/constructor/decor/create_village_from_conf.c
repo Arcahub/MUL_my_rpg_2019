4d0d9f38dc0b7597406446bfbc09391b81b3686f
Ulysse Queritet-Diop <le-ul@hotmail.fr>
2020-04-16 18:34:09 +0200

    [ADD] create_village_from_conf.c

---
+++ src/constructor/decor/create_village_from_conf.c
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_rpg_2019
+** File description:
+** create_village_from_conf
+*/
+
+#include "my_json.h"
+#include "my_game.h"
+#include "components/get_from_config.h"
+
+game_object_t *generate_map_line(game_object_t *last, json_array_t *arr)
+{
+    game_object_t *tmp = NULL;
+
+    for (int i = 0; i < arr->elem_count; i++) {
+        if (arr->array[i]->value_type == INT) {
+            tmp = generate_tile(last, *((int *) arr->array[i]->value));
+            last = (tmp) ? tmp : last;
+        }
+    }
+    return (last);
+}
+
+game_object_t *create_village(game_object_t *last, json_object_t *js,
+game_t *game, scene_t *scene)
+{
+    json_value_t *value = NULL;
+    json_array_t *arr = NULL;
+    int tile_height = 0;
+    int tile_width = 0;
+
+    if (!get_int_from_conf(js, &tile_height, "tile_height") ||
+    !get_int_from_conf(js, &tile_width, "tile_width"))
+        return (NULL);
+    value = json_get_element_by_key(js, "map");
+    if (!value || value->value_type != ARRAY)
+        return (NULL);
+    arr = value->value;
+    for (int i = 0; i < arr->elem_count; i++) {
+        if (arr->array[i]->value_type == ARRAY)
+            generate_map_line(last, arr->array[i]->value);
+    }
+    return (NULL);
+}
