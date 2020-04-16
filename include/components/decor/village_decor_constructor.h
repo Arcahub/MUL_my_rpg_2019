33384217c2687bb3e3df44c49393a765c685d129
Ulysse Queritet-Diop <le-ul@hotmail.fr>
2020-04-16 18:12:26 +0200

    [ADD] .json and .h for villages tile mapping

---
+++ include/components/decor/village_decor_constructor.h
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_rpg_2019
+** File description:
+** village_decor_constructor
+*/
+
+#ifndef VILLAGE_DECOR_CONSTRUCTOR_H_
+#define VILLAGE_DECOR_CONSTRUCTOR_H_
+
+enum {
+    SHIP,
+    GRASS,
+    PATH,
+    TREE,
+    TAVERN,
+    ARMOR,
+    WEAPON,
+    WATER,
+    PNJ1,
+    PNJ2,
+    PNJ3,
+};
+
+#endif /* !VILLAGE_DECOR_CONSTRUCTOR_H_ */
