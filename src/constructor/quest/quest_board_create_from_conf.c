88db6ba2dfeb905393e361f291d72b42898bf2e5
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2020-04-18 23:28:11 +0200

    [ADD] | Quest board destroy

--- src/constructor/quest/quest_board_create_from_conf.c
+++ src/constructor/quest/quest_board_create_from_conf.c
@@ -14,6 +14,23 @@
 #include "components/get_from_config.h"
 #include "components/quest/board.h"
 
+void rpg_quest_board_destroy(void *pt)
+{
+    quest_board_t *board = (quest_board_t *) pt;
+
+    if (board == NULL)
+        return;
+    if (board->title && board->actual_step) {
+        sfText_destroy(board->title);
+        sfText_destroy(board->actual_step);    
+        sfFont_destroy((sfFont *) sfText_getFont(board->title));
+        sfFont_destroy((sfFont *) sfText_getFont(board->actual_step));
+    }
+    for (int x = 0; x < board->number_of_step; x++)
+        if (board->steps[x])
+            sfText_destroy(board->steps[x]);
+}
+
 game_object_t *rpg_quest_board_create_from_conf(game_object_t *last, \
 json_object_t *js, game_t *game, scene_t *scene)
 {
@@ -26,6 +43,7 @@ json_object_t *js, game_t *game, scene_t *scene)
     object->draw = &rpg_quest_board_draw;
     object->extend = (void *) rpg_quest_board_create_extend(last, \
     js, game, scene);
+    object->free_extend = &rpg_quest_board_destroy;
     if (object->extend == NULL)
         return (NULL);
     return (object);
