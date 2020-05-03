5732f9559896a2339236f6666acc57de1e625f68
Arcarox <arcarox@localhost.localdomain>
2020-05-03 22:53:42 +0200

    [MOD] | fix tests

--- tests/constructors/check_box/test_check_box.c
+++ tests/constructors/check_box/test_check_box.c
@@ -12,11 +12,7 @@ Test(test_check_box, create)
 {
     json_object_t *js = json_create_from_file(
     "tests/constructors/check_box/valid.json");
-    //game_object_t *object = CONSTRUCTORS[CHECK_BOX_CONSTRUCTOR](NULL, js, NULL,
-    //NULL);
 
     cr_assert_neq(js, NULL);
-    //cr_assert_neq(object, NULL);
-    //destroy_game_object(NULL, object);
     json_object_destroy(js);
 }
